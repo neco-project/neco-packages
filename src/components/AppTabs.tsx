9f450328b38c18fa57554758adbc9dc37e42bb27
ATHAN <mheidari831003.game@gmail.com>
2025-04-12 15:33:23 -0700

    category, followup, alert, expedit tabs added

--- src/components/AppTabs.tsx
+++ src/components/AppTabs.tsx
@@ -2,24 +2,56 @@
 import { Tabs } from '@mantine/core'
 import Consult from './Consult'
 import ReAssign from './ReAssign'
-import { IconView360Arrow  , IconArrowForwardUp ,IconPlayerTrackNext} from '@tabler/icons-react'
 import Forward from './Forward'
+import Category from './Category'
+import FollowUp from './Followup'
+import Alert from './Alert'
+import Expedit from './Expedit'
+import { IconView360Arrow  , IconArrowForwardUp ,IconPlayerTrackNext, IconCategory, IconFlag, IconRotate} from '@tabler/icons-react'
 
 
 export default function AppTabs() {
   return (
     <Tabs color="red" variant="outline" defaultValue="Consult">
       <Tabs.List>
-        <Tabs.Tab value="Consult" leftSection={<IconView360Arrow stroke={1} />}>
+        <Tabs.Tab value="Category" leftSection={<IconCategory stroke={2} />}>
+          Category
+        </Tabs.Tab>
+        <Tabs.Tab value="FollowUp" leftSection={<IconFlag stroke={2} />}>
+          FollowUp
+        </Tabs.Tab>
+        <Tabs.Tab value="Alert" leftSection={<IconPlayerTrackNext stroke={2} />}>
+          Alert
+        </Tabs.Tab>
+        <Tabs.Tab value="Expedit" leftSection={<IconRotate stroke={2} />}>
+          Expedit
+        </Tabs.Tab>
+        <Tabs.Tab value="Consult" leftSection={<IconView360Arrow stroke={2} />}>
           Consult
         </Tabs.Tab>
-        <Tabs.Tab value="Reassign" leftSection={<IconArrowForwardUp stroke={1} />}>
+        <Tabs.Tab value="Reassign" leftSection={<IconArrowForwardUp stroke={2} />}>
           Reassign
         </Tabs.Tab>
-        <Tabs.Tab value="Forward" leftSection={<IconPlayerTrackNext stroke={1} />}>
+        <Tabs.Tab value="Forward" leftSection={<IconPlayerTrackNext stroke={2} />}>
           Forward
         </Tabs.Tab>
       </Tabs.List>
+
+      <Tabs.Panel value="Category" pt="xs">
+        <Category />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="FollowUp" pt="xs">
+        <FollowUp />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="Alert" pt="xs">
+        <Alert />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="Expedit" pt="xs">
+        <Expedit />
+      </Tabs.Panel>
 
       <Tabs.Panel value="Consult" pt="xs">
         <Consult />
